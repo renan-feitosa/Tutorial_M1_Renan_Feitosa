390fe77b2e942859c3c0996ccb957a8d5d83f3c1
renan-feitosa <renan.oliveira@sou.inteli.edu.br>
2023-03-02 15:18:04 -0300

    exercicio5-pages

--- SEMANA_05/02_ATIVIDADES/Semana 05 - Exercicio/Node2D.gd
+++ SEMANA_05/02_ATIVIDADES/Semana 05 - Exercicio/Node2D.gd
@@ -9,25 +9,29 @@ var lista = [] # Falta a declaração de variável (lista -> var lista)
 func _on_Button_pressed():
 	#Coletando dados inseridos pelo usuário
 	numero = int($LineEdit.text) #Os nomes das variáveis não podem conter letras com acentos (número -> numero) / É necessário um $ para chamar outro node (LineEdit -> $LineEdit)
-	nome = $LineEdit2.text #Linha escrita ao contrário e sem declaração de variável ($LineEdit2.text = nome -> var nome = $LineEdit2.text)
- 
+	nome = $LineEdit2.text #Linha escrita ao contrário e sem declaração de variável ($LineEdit2.text = nome -> v ar nome = $LineEdit2.text)
+
 
 func _on_Button2_pressed():
 	#Incrementando o número inserido pelo usuário
-	for i in range(10):
-		numero += i #variável escrita de forma errada (Numero -> numero)
-		lista.append(numero) #variável escrita de forma errada (Numero -> numero)
+	lista = []
+	for i in range(numero):
+#		numero += i #variável escrita de forma errada (Numero -> numero)
+		lista.append(i) #variável errada para append (Numero -> i)
+		i += 1
+	print(lista)
 	$Label.text = str(numero) #Transformar array numero em string
 
 
 func _on_Button3_pressed():
 	#Mudando o nome do usuário de acordo com os dados da lista
 	#Se houver algum número ímpar o nome deve adicionar "baldo" ao final
-	for i in range(len(lista)): #Mudar de while para for (while(len(lista)) -> for i in range(len(lista))
-		var cont=0 # Falta a declaração de variável (cont -> var cont)
-		var index=0 # Falta a declaração de variável (index -> var index)
+	var cont=0 # Falta a declaração de variável (cont -> var cont)
+	var i = 0
+	while i < len(lista): #Falta parâmetros para o while (while(len(lista)) -> while i < len(lista)
 		if(lista[i]%2==1):
-			nome = nome+"baldo" #Caso for impar, adiciona "baldo" (cont+=1 -> nome = nome+"baldo")
-		if(cont%2==0): #O parâmetro retornava verdadeiro se fosse diferente de zero, não com resto 0
 			cont += 1
-		$Label2.text = nome #nome foi declarado como variável local da função "on button pressed". Precisa ser declarado fora da função para esta linha funcionar
+		if(cont == 1 and lista[i]%2==1): #O parâmetro retornava verdadeiro se fosse diferente de zero, não com o necessário para re
+			nome = nome + "baldo" #Caso for impar e cont for igual a 1, adiciona "baldo"
+		i += 1
+	$Label2.text = nome #"nome" foi declarado como variável local da função "on button pressed". Precisa ser declarado fora da função para esta linha funcionar
